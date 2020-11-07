2fa5cdfd4f18f71cdce80fe1c1fe28951b1190da
lreyeswebdev <68845154+lreyeswebdev@users.noreply.github.com>
2020-11-07 11:21:26 +0800

    included additional exercises

--- activity.sql
+++ activity.sql
@@ -19,3 +19,55 @@ WHERE id = 1;
 
 DELETE FROM students
 WHERE id = 6;
+                                                                                                                                         
+SELECT COUNT (id)
+FROM students;
+
+SELECT *
+FROM students
+WHERE location = 'Manila';
+
+SELECT AVG(age)
+FROM students;
+
+SELECT *
+FROM students
+ORDER BY age DESC;
+
+CREATE TABLE classrooms (id integer, student_id integer, section VARCHAR(10));
+INSERT INTO classrooms (id, student_id, section)
+VALUES (1, 1, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (2, 2, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (3, 3, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (4, 4, 'C');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (5, 5, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (6, 6, 'A');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (7, 7, 'C');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (8, 8, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (9, 9, 'B');
+INSERT INTO classrooms (id, student_id, section)
+VALUES (10, 10, 'C');
+
+SELECT students.first_name, students.last_name, classrooms.student_id, classrooms.section
+FROM students
+INNER JOIN classrooms ON students.id=classrooms.id;
+
+SELECT students.first_name, students.last_name, classrooms.student_id, classrooms.section
+FROM students
+LEFT JOIN classrooms ON students.id=classrooms.id;
+
+SELECT students.first_name, students.last_name, classrooms.student_id, classrooms.section
+FROM students
+RIGHT JOIN classrooms ON students.id=classrooms.id;
+
+SELECT students.first_name, students.last_name, classrooms.student_id, classrooms.section
+FROM students
+FULL JOIN classrooms ON students.id=classrooms.id;
